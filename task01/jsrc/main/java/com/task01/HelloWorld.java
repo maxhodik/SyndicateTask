0c76a1538512582cf80ec44d3fed3fd598ea9aa6
m-god <m-godik@ukr.net>
2024-11-22 22:21:24 +0200

    modified lambda

--- task01/jsrc/main/java/com/task01/HelloWorld.java
+++ task01/jsrc/main/java/com/task01/HelloWorld.java
@@ -18,10 +18,10 @@ import java.util.Map;
 public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 
 	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda22");
+		System.out.println("Hello from lambda");
 		Map<String, Object> resultMap = new HashMap<String, Object>();
 		resultMap.put("statusCode", 200);
-		resultMap.put("message", "Hello from Lambda");
+		resultMap.put("body", "Hello from Lambda");
 		return resultMap;
 	}
 }
--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -2,42 +2,58 @@ package com.task02;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
 
+import java.util.HashMap;
 import java.util.Map;
 
-//@LambdaHandler(
-//        lambdaName = "hello_world",
-//        roleName = "hello_world-role",
-//        isPublishVersion = true,
+@LambdaHandler(
+        lambdaName = "hello_world",
+        roleName = "hello_world-role",
+        isPublishVersion = true,
 //        aliasName = "${lambdas_alias_name}",
-//        logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
-//)
-public class HelloWorld implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
-//
-//    public Map<String, Object> handleRequest(Object request, Context context) {
-//        System.out.println("Hello from lambda");
-//        Map<String, Object> resultMap = new HashMap<String, Object>();
-//        resultMap.put("statusCode", 200);
-//        resultMap.put("body", "Hello from Lambda");
-//        return resultMap;
-//    }
-
+        aliasName = "learn",
+        logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@LambdaUrlConfig(
+        authType = AuthType.NONE,
+        invokeMode = InvokeMode.BUFFERED
+)
+public class HelloWorld implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
     @Override
-    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent event, Context context) {
-        String httpMethod = event.getHttpMethod();
-        String path = event.getPath();
-        if (httpMethod.equalsIgnoreCase("GET")&& path.equals("/hello")){
-            return new APIGatewayProxyResponseEvent()
+    public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
+        String method = getMethod(event);
+        String path = getPath(event);
+        if (method.equalsIgnoreCase("GET" )&& path.equals("/hello")){
+            return APIGatewayV2HTTPResponse.builder()
                     .withStatusCode(200)
-                    .withBody("{\"message\": \"Hello, world!\"}")
-                    .withHeaders(Map.of("Content-Type", "application/json"));
+                    .withBody("Hello from Lambda")
+                    .build();
         }
-        String badRequestMessage= String.format("Bad request syntax or unsupported method. Request path: %s. HTTP method: %s", path, httpMethod);
-        return new APIGatewayProxyResponseEvent()
+        return APIGatewayV2HTTPResponse.builder()
                 .withStatusCode(400)
-                .withBody("{\"message\": badRequestMessage}")
-                .withHeaders(Map.of("Content-Type", "application/json"));
+                .withBody("Bad request syntax or unsupported method. Request path: {path}. HTTP method: {method}")
+                .build();
     }
+
+    private String getMethod(APIGatewayV2HTTPEvent requestEvent) {
+        return requestEvent.getRequestContext().getHttp().getMethod();
+    }
+
+    private String getPath(APIGatewayV2HTTPEvent requestEvent) {
+        return requestEvent.getRequestContext().getHttp().getPath();
+    }
+//	public Map<String, Object> handleRequest(Object request, Context context) {
+//		System.out.println("Hello from lambda task 2");
+//		Map<String, Object> resultMap = new HashMap<String, Object>();
+//		resultMap.put("statusCode", 200);
+//		resultMap.put("body", "Hello from Lambda");
+//		return resultMap;
+//	}
 }
