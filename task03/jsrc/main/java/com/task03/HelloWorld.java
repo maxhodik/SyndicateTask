ca18d7ec2602d119bc1224ac3697798059c72714
m-god <m-godik@ukr.net>
2024-11-30 16:26:04 +0200

    task 3 rebuilt v12

--- task03/jsrc/main/java/com/task03/HelloWorld.java
+++ task03/jsrc/main/java/com/task03/HelloWorld.java
@@ -17,55 +17,55 @@ import com.syndicate.deployment.model.lambda.url.InvokeMode;
 import java.util.Map;
 
 @LambdaHandler(
-        lambdaName = "hello_world",
-        roleName = "hello_world-role",
-        isPublishVersion = true,
-        aliasName = "learn",
-        logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+		lambdaName = "hello_world",
+		roleName = "hello_world-role",
+		isPublishVersion = true,
+		aliasName = "learn",
+		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 @LambdaUrlConfig(
-        authType = AuthType.NONE,
-        invokeMode = InvokeMode.BUFFERED
+		authType = AuthType.NONE,
+		invokeMode = InvokeMode.BUFFERED
 )
 public class HelloWorld implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
-    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
-    private static final ObjectMapper objectMapper = new ObjectMapper();
+	private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+	private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    @Override
-    public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
-        String method = getMethod(event);
-        String path = getPath(event);
-        if (method.equalsIgnoreCase("GET") && path.equals("/hello")) {
+	@Override
+	public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
+		String method = getMethod(event);
+		String path = getPath(event);
+		if (method.equalsIgnoreCase("GET") && path.equals("/hello")) {
 
-            return APIGatewayV2HTTPResponse.builder()
-                    .withStatusCode(200)
-                    .withHeaders(Map.of("Content-Type", "application/json"))
-                    .withBody("{\"statusCode\": 200,\"message\": \"Hello from Lambda\"}")
-                    .build();
-        }
-        String message = String.format("Bad request syntax or unsupported method. Request path: %s. HTTP method: %s",
-                path, method);
-        Map<String, Object> error = Map.of("statusCode", 400,
-                "message", message);
-        String errorMessage = null;
-        try {
-            errorMessage = objectMapper.writeValueAsString(error);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-        return APIGatewayV2HTTPResponse.builder()
-                .withStatusCode(400)
-                .withHeaders(Map.of("Content-Type", "application/json"))
-                .withBody(errorMessage)
-                .build();
-    }
+			return APIGatewayV2HTTPResponse.builder()
+					.withStatusCode(200)
+					.withHeaders(Map.of("Content-Type", "application/json"))
+					.withBody("{\"statusCode\": 200,\"message\": \"Hello from Lambda\"}")
+					.build();
+		}
+		String message = String.format("Bad request syntax or unsupported method. Request path: %s. HTTP method: %s",
+				path, method);
+		Map<String, Object> error = Map.of("statusCode", 400,
+				"message", message);
+		String errorMessage = null;
+		try {
+			errorMessage = objectMapper.writeValueAsString(error);
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException(e);
+		}
+		return APIGatewayV2HTTPResponse.builder()
+				.withStatusCode(400)
+				.withHeaders(Map.of("Content-Type", "application/json"))
+				.withBody(errorMessage)
+				.build();
+	}
 
-    private String getMethod(APIGatewayV2HTTPEvent requestEvent) {
-        return requestEvent.getRequestContext().getHttp().getMethod();
-    }
+	private String getMethod(APIGatewayV2HTTPEvent requestEvent) {
+		return requestEvent.getRequestContext().getHttp().getMethod();
+	}
 
-    private String getPath(APIGatewayV2HTTPEvent requestEvent) {
-        return requestEvent.getRequestContext().getHttp().getPath();
-    }
+	private String getPath(APIGatewayV2HTTPEvent requestEvent) {
+		return requestEvent.getRequestContext().getHttp().getPath();
+	}
 
 }
