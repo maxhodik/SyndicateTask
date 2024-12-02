2d6e467591f2d3b96d6872f77585286b0bc6c3c2
m-god <m-godik@ukr.net>
2024-12-02 23:52:19 +0200

    task 7 refactoring lambdaV4

--- task07/jsrc/main/java/com/task07/UuidGenerator.java
+++ task07/jsrc/main/java/com/task07/UuidGenerator.java
@@ -5,8 +5,6 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.google.gson.Gson;
-import com.syndicate.deployment.annotations.environment.EnvironmentVariable;
-import com.syndicate.deployment.annotations.environment.EnvironmentVariables;
 import com.syndicate.deployment.annotations.events.RuleEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
@@ -23,13 +21,8 @@ import java.util.*;
         logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 @RuleEventSource(targetRule = "uuid_trigger")
-//@EnvironmentVariables(value = {
-//        @EnvironmentVariable(key = "target_bucket", value = "${target_bucket}"),
-//        @EnvironmentVariable(key = "region", value = "${region}")
-//})
 
 public class UuidGenerator implements RequestHandler<Object, String> {
-    //    private static final String BUCKET_NAME = System.getenv("target_bucket");
     private static final String BUCKET_NAME = "cmtr-529b17ca-uuid-storage";
     public static final String BUCKET_NAME_TEST = "cmtr-529b17ca-uuid-storage-test";
     private final AmazonS3 s3Client = AmazonS3ClientBuilder.defaultClient();
@@ -47,7 +40,6 @@ public class UuidGenerator implements RequestHandler<Object, String> {
         ids.put("ids", uuidList);
         String jsonIds = gson.toJson(ids);
 
-//        s3Client.putObject(BUCKET_NAME, fileName, jsonIds);
         s3Client.putObject(BUCKET_NAME_TEST, fileName, jsonIds);
 
         return "File created" + fileName;
