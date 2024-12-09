f56a57a4c6209afacd4ab77eac363e6c21da51ce
m-god <m-godik@ukr.net>
2024-12-10 00:17:50 +0200

    task 10 v2.2

--- task10/jsrc/main/java/com/task10/ApiHandler.java
+++ task10/jsrc/main/java/com/task10/ApiHandler.java
@@ -13,7 +13,6 @@ import com.syndicate.deployment.model.ResourceType;
 import com.syndicate.deployment.model.RetentionSetting;
 import com.task10.dto.RouteKey;
 import com.task10.handler.*;
-import lombok.extern.log4j.Log4j2;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
@@ -41,7 +40,7 @@ import static com.syndicate.deployment.model.environment.ValueTransformer.USER_P
         resourceType = ResourceType.COGNITO_USER_POOL,
         name = "${booking_userpool}"
 )
-@Log4j2
+
 public class ApiHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
     private final CognitoIdentityProviderClient cognitoClient;
     private final Map<RouteKey, RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent>> handlersByRouteKey;
