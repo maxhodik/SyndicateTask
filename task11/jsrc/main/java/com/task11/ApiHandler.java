0d76b545dc3051dc8843747c7b327bf8ca8eb958
m-god <m-godik@ukr.net>
2024-12-12 20:58:59 +0200

    task 100 v300.7

--- task11/jsrc/main/java/com/task11/ApiHandler.java
+++ task11/jsrc/main/java/com/task11/ApiHandler.java
@@ -7,10 +7,13 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
 import com.syndicate.deployment.annotations.environment.EnvironmentVariable;
 import com.syndicate.deployment.annotations.environment.EnvironmentVariables;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.annotations.resources.DependsOn;
 import com.syndicate.deployment.model.DeploymentRuntime;
 import com.syndicate.deployment.model.ResourceType;
 import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
 import com.task11.dto.RouteKey;
 import com.task11.handler.*;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
@@ -39,6 +42,10 @@ import static com.syndicate.deployment.model.environment.ValueTransformer.USER_P
 @DependsOn(
         resourceType = ResourceType.COGNITO_USER_POOL,
         name = "${booking_userpool}"
+)
+@LambdaUrlConfig(
+        authType = AuthType.NONE,
+        invokeMode = InvokeMode.BUFFERED
 )
 
 public class ApiHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
